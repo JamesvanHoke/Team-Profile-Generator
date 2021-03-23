b92f5f87b99ea7f634c4353e08912ea3c49daf49
James V <james.vanhoke@yahoo.com>
2021-03-23 15:49:39 -0700

    Test Psuedo code

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,10 @@
+/* Tests
+Check if we can create an employee object,
+Check if we can assign a name to employee,
+Check if we can assign an ID to employee,
+Check if we can assign an Email to employee,
+Check if we can get a name via getName() method,
+Check if we can get a ID via getId() method,
+Check if we can get a Email via getEmail() method,
+Check if we can get a Role via getRole() method,
+*/
