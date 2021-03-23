8495f758113922c538f8c9c40310ef53fc785741
James V <james.vanhoke@yahoo.com>
2021-03-23 16:38:01 -0700

    Engineer Tests

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,49 @@
+const Engineer = require("../lib/Engineer");
+
+describe("Engineer", () => {
+  describe("Init", () => {
+    it("should be able to create an Engineer object", () => {
+      const testObj = new Engineer();
+      expect(typeof testObj).toBe("object");
+    });
+
+    it("should be able to set name, Id, Email and Github to the Engineer Object", () => {
+      const testObj = new Engineer(
+        "testName",
+        1,
+        "testEmail@email.com",
+        "testGithub"
+      );
+      
+      expect(testObj.name).toBe("testName");
+      expect(testObj.id).toBe(1);
+      expect(testObj.email).toBe("testEmail@email.com");
+      expect(testObj.github).toBe("testGithub");
+    });
+
+    describe("getMethods", () => {
+      const testObj = new Engineer(
+        "testName",
+        1,
+        "testEmail@email.com",
+        "testGithub"
+      );
+
+      it("Should return the objects name", () => {
+        expect(testObj.getName()).toBe("testName");
+      });
+      it("Should return the objects id", () => {
+        expect(testObj.getId()).toBe(1);
+      });
+      it("Should return the objects email", () => {
+        expect(testObj.getEmail()).toBe("testEmail@email.com");
+      });
+      it("should return the objects github", () => {
+        expect(testObj.getEmail()).toBe("testGithub");
+      });
+      it("Should return the objects role", () => {
+        expect(testObj.getRole()).toBe("Engineer");
+      });
+    });
+  });
+});
