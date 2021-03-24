e5eb65234221ee4f84a0eb77438ed872a2b2cbb1
James V <james.vanhoke@yahoo.com>
2021-03-23 21:23:04 -0700

    added jQuery so Bootstrap functions without error

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const HTMLTemplate = `<!DOCTYPE html>
 
 const HTMLTemplateEnd = `
 </div>
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
 </html>`;
