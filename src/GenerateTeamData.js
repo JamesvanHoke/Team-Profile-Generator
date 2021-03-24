2ac64119ab74b183058c556df1fd9f3c1691c6a6
James V <james.vanhoke@yahoo.com>
2021-03-23 20:42:10 -0700

    Async functionality

--- src/GenerateTeamData.js
+++ src/GenerateTeamData.js
@@ -4,63 +4,68 @@ const Intern = require("../lib/Intern");
 const inquirer = require("inquirer");
 
 const userQuestions = [
-    {
-        type: "list",
-        message: "Enter the employees role:",
-        name: "role",
-        choices: ["Manager", "Engineer", "Intern"]
-    },
-    {
-        type: "input",
-        message: "Enter the employees Name:",
-        name: "name"
-    },
-    {
-        type: "input",
-        message: "Enter the employees ID:",
-        name: "id"
-    },
-    {
-        type: "input",
-        message: "Enter the employees Email:",
-        name: "email"
-    },
-    {
-        type: "input",
-        message: "Enter the Manager's office number:",
-        name: "officeNumber",
-        when: data => data.role === "Manager"
-    },
-    {
-        type: "input",
-        message: "Enter the Engineer's Github:",
-        name: "github",
-        when: data => data.role === "Engineer"
-    },
-    {
-        type: "input",
-        message: "Enter the Intern's School:",
-        name: "school",
-        when: data => data.role === "Intern"
-    },
-]
+  {
+    type: "list",
+    message: "Enter the employees role:",
+    name: "role",
+    choices: ["Manager", "Engineer", "Intern"]
+  },
+  {
+    type: "input",
+    message: "Enter the employees Name:",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: "Enter the employees ID:",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: "Enter the employees Email:",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: "Enter the Manager's office number:",
+    name: "officeNumber",
+    when: (data) => data.role === "Manager",
+  },
+  {
+    type: "input",
+    message: "Enter the Engineer's Github:",
+    name: "github",
+    when: (data) => data.role === "Engineer",
+  },
+  {
+    type: "input",
+    message: "Enter the Intern's School:",
+    name: "school",
+    when: (data) => data.role === "Intern",
+  },
+];
 
-function generateTeamData() {
-    let teamData;
-    inquirer.prompt(userQuestions).then((data) => {
-        switch (data.role) {
-            case "Manager":
-                TeamData = new Manager(data.name, data.id, data.email, data.officeNumber)
-                break;
-            case "Engineer":
-                TeamData = new Engineer(data.name, data.id, data.email, data.github)
-                break;
-            case "Intern":
-                TeamData = new Intern(data.name, data.id, data.email, data.school)
-                break;
-        }
-    })
-    return teamData
+async function generateTeamData() {
+  let teamData;
+  await inquirer.prompt(userQuestions).then((data) => {
+    switch (data.role) {
+      case "Manager":
+        teamData = new Manager(
+          data.name,
+          data.id,
+          data.email,
+          data.officeNumber
+        );
+        break;
+      case "Engineer":
+        teamData = new Engineer(data.name, data.id, data.email, data.github);
+        break;
+      case "Intern":
+        teamData = new Intern(data.name, data.id, data.email, data.school);
+        break;
+    }
+  });
+  return teamData;
 }
 
-module.exports = generateTeamData
+module.exports = generateTeamData;
