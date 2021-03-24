eac08ed2fde94997aa218b9067f6167c6a88ab24
James V <james.vanhoke@yahoo.com>
2021-03-23 21:10:49 -0700

    BootStrap Classes

--- src/GenerateHTML.js
+++ src/GenerateHTML.js
@@ -15,19 +15,21 @@ const generatedEmployeeCard = function (employee) {
         break;
     }
   
-  return`<div class="card">
-  <div class="card-header">
-      <h2 class="card-name">${employee.getName()}</h3>
-      <h3 class="card-role">${employee.getRole()}</h4>
+  return`
+  <div class="card col-12 col-md-6 col-lg-4 m-4">
+  <div class="card-header bg-primary text-light">
+      <h3 class="card-name">${employee.getName()}</h3>
+      <h4 class="card-role">${employee.getRole()}</h4>
   </div>
   <div class="card-body">
       <ul class="card-contents">
           <li class="card-info">ID: ${employee.getId()}</li>
-          <li class="card-info"><a href="mailto:${employee.getEmail()}">Email: ${employee.getEmail()}</a></li>
+          <li class="card-info">Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a></li>
           <li class="card-info">${roleSpecific}</li>
       </ul>
   </div>
-</div>;`
+</div>
+`
 }
 
 module.exports = generatedEmployeeCard;
